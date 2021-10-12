8dd43963302273d8d851a2ca411def226973fbd7
Olena Hural <o.hural89@gmail.com>
2021-10-12 13:51:29 +0300

    hw6 - task 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemList = document.querySelector("#ingredients")
+
+const listOfItems = ingredients.forEach((ingredient) => {
+
+  const itemOfList = document.createElement("li")
+        itemOfList.classList.add("item")
+        itemOfList.textContent = ingredient
+
+  itemList.appendChild(itemOfList)
+})
+
+console.log(listOfItems);
+
