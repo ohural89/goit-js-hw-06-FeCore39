ba3bae87fab7d8f646b71ea5d96b5565bde56f2e
Olena Hural <o.hural89@gmail.com>
2021-10-17 11:06:08 +0300

    hw6 - task2 - other variant

--- js/task-02.js
+++ js/task-02.js
@@ -9,14 +9,27 @@ const ingredients = [
 
 const itemList = document.querySelector("#ingredients")
 
-const listOfItems = ingredients.forEach((ingredient) => {
+// const listOfItems = ingredients.forEach((ingredient) => {
 
-  const itemOfList = document.createElement("li")
-        itemOfList.classList.add("item")
-        itemOfList.textContent = ingredient
+//   const itemOfList = document.createElement("li")
+//         itemOfList.classList.add("item")
+//         itemOfList.textContent = ingredient
 
-  itemList.appendChild(itemOfList)
-})
+//   itemList.appendChild(itemOfList)
+// })
 
-console.log(listOfItems);
+// console.log(listOfItems);
 
+const markup = (item) 
+=> {
+  const element = document.createElement(`li`);
+  element.textContent = item;
+
+  return element;
+};
+
+const createMarkup = ingredients.map(markup);
+itemList.append(...createMarkup);
+
+
+// console.log(createMarkup);
