197e773a2795793a18301ee2ce89e97b97e3af05
Olena Hural <o.hural89@gmail.com>
2021-10-17 10:54:41 +0300

    hw6 - task 1 - done + needs fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,23 @@
-const listOfItems = document.querySelector("#categories")
-console.log("listOfItems", listOfItems);
+const listOfItems = document.querySelectorAll("#categories .item")
+
+console.log(`Number of categories: ${listOfItems.length}`);
+
+categories.forEach((item) =>
+console.log(
+    `Category: ${item.firstElementChild.textContent}
+    Element: ${lastElementChild.children.length}`)
+);
+
+// console.log("listOfItems", listOfItems);
+
+
+//  Number of categories: 3 
+
+// Category: Animals item.firstElementChild.textContent
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
